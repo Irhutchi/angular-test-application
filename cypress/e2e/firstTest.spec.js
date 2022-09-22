d86672c3b19b0baff8792366e1b1440908e4dfae
Ian Hutchinson <ianhutch90@gmail.com>
2022-09-22 17:01:30 +0100

    Automate the flow of creating a new article.
    Using cy.intercept to intercept the api call made after the POST
    request is sent. Assertions used to verify the info sent in the
    request matches the response.

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -5,7 +5,30 @@ describe('Test with backend', () => {
         cy.loginToApplication()
     })
 
-    it('should log in', () => {
-        cy.log('Successfully logged in, Woo!!')
+    it.only('verify correct request and response', () => {
+        // intercept the api call when the publish article button is selected.
+        cy.intercept('POST', 'https://api.realworld.io/api/articles/').as('postArticles')
+       
+        // script to create a new article
+        cy.contains('New Article').click()
+        cy.get('[formcontrolname="title"]').type('User from Ireland')
+        cy.get('[formcontrolname="description"]').type('Breezing through the course')
+        cy.get('[formcontrolname="body"]').type('in 2022')
+        cy.get('[placeholder="Enter tags"]').type('#learning')
+        cy.contains('Publish Article').click()
+        // force Cypress to wait for the api call to complete before we start looking into it.
+        cy.wait('@postArticles').then( xhr => {
+            // asert the xhr obj which contains all the info related to the api call which has the request/repsonse obj.
+            console.log(xhr)
+            expect(xhr.response.statusCode).to.equal(200)
+            expect(xhr.request.body.article.body).to.equal('in 2022')
+            expect(xhr.response.body.article.description).to.equal('Breezing through the course')
+        })
+
+        // Delete the article to allow the user to run this test again without error
+        
+
     })
 })
+
+
